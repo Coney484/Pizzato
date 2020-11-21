3d7629e0fae7b52a716d752fe0da8a97e7ba362b
ROHAN CHOUDHARY <anandc.rohan484@gmail.com>
2020-11-21 22:36:18 +0530

    worked on header part of home screen

--- lib/Helpers/headers.dart
+++ lib/Helpers/headers.dart
@@ -12,8 +12,10 @@ class Headers extends ChangeNotifier {
           IconButton(icon: Icon(FontAwesomeIcons.userAlt), onPressed: () {}),
           Row(
             children: [
-              Icon(FontAwesomeIcons.locationArrow,
-              color: Colors.grey.shade600,),
+              Icon(
+                FontAwesomeIcons.locationArrow,
+                color: Colors.grey.shade600,
+              ),
               Text(
                 'New Police Area',
                 style: TextStyle(
@@ -33,4 +35,101 @@ class Headers extends ChangeNotifier {
       ),
     );
   }
+
+  Widget headerText() {
+    return Container(
+      constraints: BoxConstraints(maxWidth: 250.0),
+      child: RichText(
+        text: TextSpan(
+            text: 'What would you like ',
+            style: TextStyle(
+                fontWeight: FontWeight.w300,
+                color: Colors.black,
+                fontSize: 29.0),
+            children: <TextSpan>[
+              TextSpan(
+                  text: 'to eat ?',
+                  style: TextStyle(
+                      fontSize: 46.0,
+                      fontWeight: FontWeight.w600,
+                      color: Colors.black))
+            ]),
+      ),
+    );
+  }
+
+  Widget headerMenu(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.only(top: 20.0),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+          GestureDetector(
+            onTap: () {},
+            child: Container(
+              decoration: BoxDecoration(
+                boxShadow: [
+                  BoxShadow(color: Colors.redAccent, blurRadius: 15.0),
+                ],
+                borderRadius: BorderRadius.all(Radius.circular(50.0)),
+                color: Colors.grey.shade100,
+              ),
+              height: 40.0,
+              width: 100.0,
+              child: Center(
+                child: Text(
+                  'All Food',
+                  style:
+                      TextStyle(color: Colors.black, fontWeight: FontWeight.bold),
+                ),
+              ),
+            ),
+          ),
+          GestureDetector(
+            onTap: () {},
+            child: Container(
+              decoration: BoxDecoration(
+                boxShadow: [
+                  BoxShadow(color: Colors.blueAccent, blurRadius: 15.0),
+                ],
+                borderRadius: BorderRadius.all(Radius.circular(50.0)),
+                color: Colors.grey.shade100,
+              ),
+              height: 40.0,
+              width: 100.0,
+              child: Center(
+                child: Text(
+                  'Pizza',
+                  style:
+                      TextStyle(color: Colors.black, fontWeight: FontWeight.bold),
+                ),
+              ),
+            ),
+          ),
+          GestureDetector(
+            onTap: () {},
+            child: Container(
+              decoration: BoxDecoration(
+                boxShadow: [
+                  BoxShadow(color: Colors.greenAccent, blurRadius: 15.0),
+                ],
+                borderRadius: BorderRadius.all(Radius.circular(50.0)),
+                color: Colors.grey.shade100,
+              ),
+              height: 40.0,
+              width: 100.0,
+              child: Center(
+                child: Text(
+                  'Pasta',
+                  style:
+                      TextStyle(color: Colors.black, fontWeight: FontWeight.bold),
+                ),
+              ),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+  
 }
